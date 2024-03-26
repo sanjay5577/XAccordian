234ccee872220a50f5e4dbe75e91abb30bb83430
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-03-26 15:52:51 +0000

    accordian

---
+++ accordian/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <section className='section'>
+        <details>
+          <summary>Accordian Menu 1</summary>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </details>
+
+        <details>
+          <summary>Accordian Menu 2</summary>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </details>
+
+        <details>
+          <summary>Accordian Menu 3</summary>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </details>
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
